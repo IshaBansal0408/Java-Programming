0aa9c423b0b7131a7a1c1930050abf2d268cf347
IshaBansal0408 <isha.bansal1504@gmail.com>
2021-01-23 03:36:02 +0530

    more codes

---
+++ 20. Extra concepts/3. Operations/src/Arithmetic.java
@@ -0,0 +1,12 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 25, b = 13;
+        System.out.print("First number: "+a+"\n");
+        System.out.print("Second number: "+b+"\n");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+    }
+}
